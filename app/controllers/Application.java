d6f6f59d11d935d116fa6967a510fa32f854ed07
ttreitlinger <ttreitlinger@gmail.com>
2010-11-19 17:30:14 +0000

    comment form validation and js sugar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -27,9 +28,14 @@ public class Application extends Controller {
     	render(post);
     }
 
-    public static void postComment(Long postId, String author, String content) {
+    public static void postComment(Long postId, @Required String author, @Required String content) {
     	Post post = Post.findById(postId);
+    	
+    	if (validation.hasErrors())
+    		render("Application/show.html", post);
+    	
     	post.addComment(author, content);
+    	flash.success("Thanks for posting %s", author);
     	show(postId);
     }
 }
