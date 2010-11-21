2cdee0cfac882d76210a09bada48f5a7e107a631
ttreitlinger <ttreitlinger@gmail.com>
2010-11-21 20:22:29 +0000

    add tagging of posts and display all posts tagged with a given tag.

--- app/models/Post.java
+++ app/models/Post.java
@@ -3,11 +3,14 @@ package models;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import play.db.jpa.Model;
@@ -27,12 +30,17 @@ public class Post extends Model {
 	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
 	public List<Comment> comments;
 	
+	@ManyToMany(cascade=CascadeType.PERSIST)
+	public Set<Tag> tags;
+	
+	
 	public Post(User author, String title, String content) {
 		this.author = author;
 		this.title = title;
 		this.content = content;
 		this.postedAt = new Date();
 		this.comments = new ArrayList<Comment>();
+		this.tags = new TreeSet<Tag>();
 	}
 	
 	public Post addComment(String author, String content) {
@@ -49,4 +57,21 @@ public class Post extends Model {
 	public Post next() {
 		return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
 	}
+	
+	public Post tagItWith(String name) {
+	  tags.add(Tag.findOrCreateByName(name));
+	  return this;
+	}
+	
+	public static List<Post> findTaggedWith(String name) {
+	  return Post.find(
+	    "select distinct p from Post p join p.tags as t where t.name = ?", name
+	  ).fetch();
+	}
+	
+	public static List<Post> findTaggedWith(String... tags) {
+      return Post.find(
+              "select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content, p.postedAt having count(t.id) = :size"
+      ).bind("tags", tags).bind("size", tags.length).fetch();
+  }
 }
