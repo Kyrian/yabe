21204dd126fbcf1d90ed6357c71331a841843da7
ttreitlinger <ttreitlinger@gmail.com>
2010-11-21 18:31:05 +0000

    validate captcha when comments form is submitted

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.data.validation.Required;
+import play.libs.Codec;
 import play.libs.Images;
 import play.mvc.*;
 
@@ -26,22 +28,35 @@ public class Application extends Controller {
     
     public static void show(Long id) {
     	Post post = Post.findById(id);
-    	render(post);
+    	String randomId = Codec.UUID();
+    	render(post, randomId);
     }
 
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+    public static void postComment(
+            Long postId, 
+            @Required(message="Author is required") String author, 
+            @Required(message="A message is required") String content, 
+            @Required(message="Please type the code") String code, 
+            String randomID) 
+    {
+
     	Post post = Post.findById(postId);
     	
+    	validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again.");
+    	
     	if (validation.hasErrors())
-    		render("Application/show.html", post);
+    		render("Application/show.html", post, randomID);
     	
     	post.addComment(author, content);
     	flash.success("Thanks for posting %s", author);
+    	Cache.delete(randomID);
     	show(postId);
     }
     
-    public static void captcha() {
+    public static void captcha(String id) {
     	Images.Captcha captcha = Images.captcha();
+    	String code = captcha.getText("#e4eafd");
+    	Cache.set(id, code, "10mn");
     	renderBinary(captcha);
     }
 }
